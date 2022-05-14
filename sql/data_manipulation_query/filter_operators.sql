097dc38421194e73624e4309c23a39d69f48c270
Valéria Ferreira <71043809+valferreiraalv@users.noreply.github.com>
2022-05-14 18:58:59 -0300

    Add files

---
+++ sql/data_manipulation_query/filter_operators.sql
@@ -0,0 +1,18 @@
+SELECT * FROM tbcliente;
+
+SELECT * FROM tbcliente WHERE IDADE = 22;
+
+SELECT * FROM tbcliente WHERE IDADE > 22;
+
+SELECT * FROM tbcliente WHERE IDADE < 22;
+
+SELECT * FROM tbcliente WHERE IDADE <= 22;
+
+SELECT * FROM tbcliente WHERE IDADE <> 22;
+
+SELECT * FROM tbcliente WHERE IDADE >= 'Fernando Cavalcante'
+
+SELECT * FROM tbcliente WHERE IDADE <> 'Fernando Cavalcante'
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009;
+
