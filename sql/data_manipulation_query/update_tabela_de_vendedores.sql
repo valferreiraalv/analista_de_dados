5cc2d2396c7bb9690cd45588423afa161b695561
Valéria Ferreira <71043809+valferreiraalv@users.noreply.github.com>
2022-05-14 15:01:00 -0300

    Add files

---
+++ sql/data_manipulation_query/update_tabela_de_vendedores.sql
@@ -0,0 +1,7 @@
+USE sucos; 
+
+UPDATE tabela_de_vendedores SET PERCENTUAL_COMISSAO = 0.11
+WHERE MATRICULA = '00236';
+
+UPDATE tabela_de_vendedores SET NOME = 'Jose Geraldo da Fonseca Junior' 
+WHERE MATRICULA = '00233';
